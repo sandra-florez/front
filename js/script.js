64ac1b04284c6267adaec65359b8e5fb7e8201fb
rinwer <rinsonjawer@gmail.com>
2022-09-19 22:55:06 -0500

    Actulzacion front

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function hizoClick() {
+  alert("Sending information");
+}
+
+let bottom = document.getElementById("bottom");
+let text = document.getElementById("modificarText");
+
+bottom.addEventListener("click", hizoClick);
